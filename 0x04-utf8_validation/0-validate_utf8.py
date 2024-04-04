b9fe970839bb44a20fb09807b8cc3906ff14f044
mcakyerima <mcakyerima@gmail.com>
2024-04-04 06:28:31 +0100

    ✅ finished challenge

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+""" A method that determines if a given data set represents a valid UTF-8 encoding."""
+
 
 def validUTF8(data):
     """Determines if a given data set represents a valid UTF-8 encoding.
